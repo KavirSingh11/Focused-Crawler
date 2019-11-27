2946045eba161d70cd7aabc8f89dd3793b36c90f
Nigel Fernandes <fernandes.nigelj@gmail.com>
2019-11-27 15:41:55 -0500

    extract meta data from url

--- spider.py
+++ spider.py
@@ -1,5 +1,5 @@
 import bs4  
-from urllib.request import urlopen as uReq
+from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
 
 class Spider:
@@ -14,3 +14,34 @@ class Spider:
     
     def run(self):
         print("crawling...")
+        self.getUrlData(self.urlQueue[0])
+
+    def getUrlData(self, url):
+        client = urlopen(url)
+        webpage = client.read()
+        client.close()
+        
+        pageSoup = soup(webpage, "html.parser")
+        
+        title = str(pageSoup.title)
+        desc = self.getAttributeData(pageSoup, "meta", "description")
+        author = self.getAttributeData(pageSoup, "meta", "author")
+        keywords = self.getAttributeData(pageSoup, "meta", "keywords")
+
+        links = pageSoup.find_all("a", href=True)
+
+        print("url: " + url)
+        print("title: " + title)
+        print("desc: " + desc)
+        print("author: " + author)
+        print("keywords: " + keywords)
+        print("links: " + str(links))
+
+    def getAttributeData(self, pageSoup, tag, attr):
+        data = pageSoup.find(tag, attrs={ "name": attr })
+        if data:
+            return str(data["content"])
+        else:
+            return "No " + attr + " given."
+
+
