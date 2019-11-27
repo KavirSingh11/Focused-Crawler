ba807df185f20934cd7b6ed9dfc897eacbe8a87c
Nigel Fernandes <fernandes.nigelj@gmail.com>
2019-11-27 17:09:21 -0500

    extract url and content from <a>

--- spider.py
+++ spider.py
@@ -17,31 +17,57 @@ class Spider:
         self.getUrlData(self.urlQueue[0])
 
     def getUrlData(self, url):
-        client = urlopen(url)
-        webpage = client.read()
-        client.close()
-        
-        pageSoup = soup(webpage, "html.parser")
-        
-        title = str(pageSoup.title)
-        desc = self.getAttributeData(pageSoup, "meta", "description")
-        author = self.getAttributeData(pageSoup, "meta", "author")
-        keywords = self.getAttributeData(pageSoup, "meta", "keywords")
+        client = None
+        try:
+            client = urlopen(url)
+            webpage = client.read()
+            client.close()
+            
+            pageSoup = soup(webpage, "html.parser")
+            
+            title = pageSoup.title.string
+            desc = self.getAttributeData(pageSoup, "meta", "description")
+            author = self.getAttributeData(pageSoup, "meta", "author")
+            keywords = self.getAttributeData(pageSoup, "meta", "keywords")
 
-        links = pageSoup.find_all("a", href=True)
+            links = pageSoup.find_all("a", href=True)
+            # extracts url and content from <a> tag
+            linkData = [self.getLinkData(link) for link in links]
 
-        print("url: " + url)
-        print("title: " + title)
-        print("desc: " + desc)
-        print("author: " + author)
-        print("keywords: " + keywords)
-        print("links: " + str(links))
+            # filter relevant links
+            relevantLinks = linkData
 
+            print("---- START ----")
+            print("url: " + url)
+            print("title: " + title)
+            print("desc: " + desc)
+            print("author: " + author)
+            print("keywords: " + keywords)
+            print("relevant links: " + str(relevantLinks))
+            print("---- END ----")
+        except:
+            print('Could not visit url.')
+        finally:
+            if client != None:
+                client.close()
+                
     def getAttributeData(self, pageSoup, tag, attr):
         data = pageSoup.find(tag, attrs={ "name": attr })
         if data:
             return str(data["content"])
         else:
             return "No " + attr + " given."
+
+    def getLinkData(self, link):
+        url = link["href"]
+        content =  self.getLinkDataContents(link.contents)
+
+        return (url, content)
+    
+    def getLinkDataContents(self, contents):
+        for content in contents:
+            if isinstance(content, str):
+                return content
+        
 
 
