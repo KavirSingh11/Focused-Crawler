e7be5ead56a52d6084a1a264f9799cfc4a77fbcf
Nigel Fernandes <fernandes.nigelj@gmail.com>
2019-11-27 14:06:36 -0500

    init

--- runner.py
+++ runner.py
@@ -1,7 +1,9 @@
 import scraper
 
-query = input("Enter your topic")
+# query = input("Enter your topic")
+query = 'html'
+url = 'https://www.reddit.com/r/all/'
 
-url = input("Enter the starting the URL")
+# url = input("Enter the starting the URL")
 pageSoup = scraper.getPageData(url)
 links = scraper.searchLinks
